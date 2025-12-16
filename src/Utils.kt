8972a644e3002b36f9195767013b12ef0b9f324e
Michael Friend <16885048+mrf7@users.noreply.github.com>
2025-12-16 16:31:00 -0500

    day 6

--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,7 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun <T> T.alsoPrint(block: (T) -> Any = { this.toString() }) = also { println(block(this)) }
+inline fun <F, S, F2> List<Pair<F, S>>.mapFirst(block: (F) -> F2) = map { block(it.first) to it.second }
+inline fun <F, S, S2> List<Pair<F, S>>.mapSecond(block: (S) -> S2) = map { it.first to block(it.second) }
